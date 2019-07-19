33327cd64c5716cf93664371813c5ac44039e362
Aliaksey Aksionchyk <aliaksey.aksionchyk@leverx.com>
2019-07-19 15:02:57 +0300

    Add install script and modify Makefile to execute install; Add 'sap-vpn' script that could be added in PATH to run vpn

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ -f /usr/bin/sap-vpn ]]; then
+    echo Already insatlled.
+    exit 0
+fi
+
+HOST='https://connectwdf11.sap.com'
+CUSER=''
+PIN=''
+
+read -p "Enter VPN host to connect[Press enter to use default = $HOST]:"
+if [[ -n $REPLY ]]; then
+    HOST=$REPLY
+fi
+read -p "Enter your C/D/I-user:"
+CUSER="$REPLY"
+read -p "Enter your PIN[4 digits]:"
+PIN=$REPLY
+CWD="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+
+FILE=$CWD/sap-vpn
+sed -i "s,{{CWD}},$CWD,g" $FILE
+sed -i "s/{{CUSER}}/$CUSER/g" $FILE
+sed -i "s,{{HOST}},$HOST,g" $FILE
+sed -i "s/{{PIN}}/$PIN/g" $FILE
+
+chmod a+x sap-vpn
+sudo cp sap-vpn /usr/bin/ 
+
