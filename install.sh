2ed4a33ce7bb1d7f4ed5f5317636c574d961d21d
Aliaksey Aksionchyk <aliaksey.aksionchyk@leverx.com>
2019-07-29 14:10:10 +0300

    Add reconfigure option in install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
+# SOURCE DIRECTORY
+CWD="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+
 if [[ -f /usr/bin/sap-vpn ]]; then
     echo Already insatlled.
-    exit 0
+    read -p 'Would you like to change configuration?'
+    if [[ $REPLY =~ '[y|Y|([y|Y]es)]' ]]; then
+        git -C $CWD checkout -f master
+    else
+        exit 0
+    fi
 fi
 
 HOST='https://connectwdf11.sap.com'
 CUSER=''
 PIN=''
+MODE='gateway'
 
+# HOST
 read -p "Enter VPN host to connect[Press enter to use default = $HOST]:"
 if [[ -n $REPLY ]]; then
     HOST=$REPLY
 fi
+
+# USER
 read -p "Enter your C/D/I-user:"
 CUSER="$REPLY"
+
+# PIN
 read -p "Enter your PIN[4 digits]:"
 PIN=$REPLY
+
+# MODE
 read -p "Enter default mode for VPN connection[client|gateway]:"
-MODE="${REPLY}"
-CWD="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+if [[ -n $REPLY ]]; then
+    MODE="${REPLY}"
+fi
 
 FILE=$CWD/sap-vpn
 sed -i "s,{{CWD}},$CWD,g" $FILE
